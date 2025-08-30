fce92b450f14d6747c568c5d1f87bfda2d614af5
Asad Ali <120159273+asadalimcj@users.noreply.github.com>
2025-08-30 08:04:30 +0500

    Update app.js
    added path for static files

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Session configuration
 app.use(session({
